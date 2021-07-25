d97335f78b2f60674e0a1921290b6397bd6e66ea
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-07-25 19:45:57 +0300

    Add feed template.

--- neighborhood/urls.py
+++ neighborhood/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns =[
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('feed/', views.feed, name='feed'),
 ]
--- neighborhood/views.py
+++ neighborhood/views.py
@@ -5,4 +5,7 @@ def index(request):
 
     return render(request, 'neighborhood/index.html')
 
+def feed(request):
+
+    return render(request, 'neighborhood/feed.html')
 
