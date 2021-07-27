be928e59169476f31affb3ae1646aa4f97a039d8
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-07-27 16:34:42 +0300

    Add allowed hosts.

--- nhproject/settings.py
+++ nhproject/settings.py
@@ -105,7 +105,7 @@ else:
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['neighborh00d.herokuapp.com','127.0.0.1']
 
 
 # Password validation
